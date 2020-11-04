a9aab80a15893a1d651806fe317a51c3a71cf0bb
clayton1549 <pereira.cleiton.oliver@gmail.com>
2020-11-04 15:26:00 -0300

    User Update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -10,40 +10,51 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
 
-
 @Service
 public class UserService {
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
-	
-	return repository.findAll();
-	
+
+	public List<User> findAll() {
+
+		return repository.findAll();
+
 	}
-	
+
 	public User findById(Long id) {
-	Optional<User> obj = 	repository.findById(id);
-	
-	return obj.get();
-	
-	
-		
+		Optional<User> obj = repository.findById(id);
+
+		return obj.get();
+
 	}
-	
+
 	public User insert(User obj) {
-		
-		return  repository.save(obj);
-		
-		
+
+		return repository.save(obj);
+
 	}
 	
+	public User update(Long id , User obj) {
+	
+		User entity = repository.getOne(id);
+		updateData(entity , obj);
+		return repository.save(entity);
+		
+		
+		
+	
+	}
+
+	private void updateData(User entity, User obj) {
+	   entity.setName(obj.getName());
+	   entity.setEmail(obj.getEmail());
+	   entity.setPhone(obj.getPhone());
+	   
+		
+	}
+
 	public void delete(Long id) {
-		
-		
+
 	}
-	
-	
-	
 
 }
