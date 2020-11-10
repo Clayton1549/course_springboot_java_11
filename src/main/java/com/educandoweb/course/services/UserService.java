ee7d849375f4394aa5cdb7ad142fac5a989b6127
clayton1549 <pereira.cleiton.oliver@gmail.com>
2020-11-10 16:08:33 -0300

    Exception handling - findById

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -5,10 +5,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -24,7 +24,7 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 
-		return obj.get();
+		return obj.orElseThrow(() ->  new ResourceNotFoundException(id)) ;
 
 	}
 
@@ -54,6 +54,7 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
+		repository.deleteById(id);
 
 	}
 
